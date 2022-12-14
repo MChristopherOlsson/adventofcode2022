cb14bbdeec4cd927f3d9a3a7f0c24ccbbc70a351
Christopher Olsson <m.christopher.olsson@gmail.com>
2022-12-14 15:16:32 +0000

    Add solution 2

--- 3/solution.py
+++ 3/solution.py
@@ -1,13 +1,26 @@
 letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-points = 0
+p1, p2, count = 0, 0, 0
+lines = []
 
-for data in open("input.txt"):
-    lines = data.splitlines()
-    for l in lines:
-        m = len(l) // 2
-        firstHalf, secondHalf = set(l[:m]), set(l[m:])
-        c = firstHalf.intersection(secondHalf).pop()
-        points += letters.find(c) + 1
-                
-print(points)
+for line in open("input.txt"):
+    l = line.splitlines()[0]
+    lines.append(l)
+    count += 1
+    
+    # Solution 1
+    m = len(l) // 2
+    firstHalf, secondHalf = set(l[:m]), set(l[m:])
+    c = firstHalf.intersection(secondHalf).pop()
+    p1 += letters.find(c) + 1
+
+    # Solution 2
+    if count % 3 == 0:
+        x = set(lines[count - 3])
+        y = set(lines[count - 2])
+        z = set(lines[count - 1])
+        c = x.intersection(y,z).pop()
+        p2 += letters.find(c) + 1
+
+print(f"Solution 1: {p1}")
+print(f"Solution 2: {p2}")
 
